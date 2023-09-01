c8ae65011a32fc1d37217b9f8256b6e4866fd5f4
Anthony Fort <ajf2195@columbia.edu>
2023-09-01 16:34:25 +0100

    started nav bar, created components folder for styling, edited nav styling and imported it to main.scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,38 @@
+import Carousel from 'react-bootstrap/Carousel'
+// import ExampleCarouselImage from 'components/ExampleCarouselImage'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100"
+          src="https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+          alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100"
+          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          alt="First slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100"
+          src="https://food-images.files.bbci.co.uk/food/recipes/basic_white_bread_18916_16x9.jpg"
+          alt="First slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,33 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import Logo from '../images/logo.png'
+
+import Modal from 'react-bootstrap/Modal'
+
+
 export default function Nav() {
-  return <h1>Nav</h1>
+
+  const [show, setShow] = useState(false)
+
+  return (
+    <>
+      <nav className="header-nav">
+        <Link to="/"><img src={Logo} className="nav-logo" alt="AFJH logo" /></Link>
+        <div className="nav-toggle" onClick={() => setShow(true)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+      {/* <Modal show={show} fullscreen={true} onHide={() => setShow(false)}>
+        <Modal.Header className="centred text-center" closeButton>
+          <nav onClick={() => setShow(false)}>
+            <Link className="" to="/categoriesList">Categories</Link>
+
+          </nav>
+        </Modal.Header>
+      </Modal> */}
+    </>
+  )
 }
