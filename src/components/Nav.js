a1330e881e619cffde152c326039b6765b8a29c7
Anthony Fort <ajf2195@columbia.edu>
2023-09-01 19:39:06 +0100

    added notes to Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,6 +12,7 @@ export default function Nav() {
   const navigate = useNavigate()
   const [categories, setCategories] = useState([])
 
+  // This function fetches a list of categories and sets them to the "categories" state.
   useEffect(() => {
     async function getItemsData() {
       try {
@@ -24,8 +25,10 @@ export default function Nav() {
     getItemsData()
   }, [])
 
+  // This function is triggered whenever an item in the dropdown menu is clicked
+  // Based on which dropdown item is clicked, it receives a 'category'
+  // It navigates the user to the categories page and, in addition, sends the name of the category over with it
   function handleClick(category) {
-    console.log(category)
     navigate(
       '/categories',
       {
@@ -44,6 +47,7 @@ export default function Nav() {
             Dropdown Button
           </Dropdown.Toggle>
           <Dropdown.Menu>
+            {/* For every item in the categories array, a dropdown item is created */}
             {categories.length > 0 ?
               categories.map(category => {
                 return <Dropdown.Item key={category} onClick={() => handleClick(category)}>{category}</Dropdown.Item>
