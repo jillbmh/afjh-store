75c7079cf5d9edfdb43c81aa0beea1269ba1c2e1
Anthony Fort <ajf2195@columbia.edu>
2023-09-01 18:47:46 +0100

    updated CategoriesList so that, when people navigate there, it shows the items from the selected category

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,8 +1,56 @@
+import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import axios from 'axios'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function CategoriesList() {
 
   const { state } = useLocation()
+  const [items, setItems] = useState([])
 
-  return <h1>{state.category}</h1>
+  useEffect(() => {
+    async function getItemsData() {
+      try {
+        const { data } = await axios.get('/products')
+        setItems(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getItemsData()
+  }, [state])
+
+  return (
+    <>
+      <h1>{state.category}</h1>
+      {items.length > 0 ?
+        <Container fluid>
+          <Row>
+            {
+              items.filter(item => item.category === state.category)
+                .map(item => {
+                  return (
+                    <Col
+                      className="item"
+                      xs="6"
+                      md="4"
+                      lg="3"
+                      key={item.id}
+                      style={{ backgroundImage: `url(${item.image})` }}
+                    >
+                      {item.title}
+                    </Col>
+                  )
+                })
+            }
+          </Row>
+        </Container >
+        :
+        'ERROR'
+      }
+    </>
+
+  )
 }
